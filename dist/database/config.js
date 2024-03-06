4438577232dc01ef157c0ffddf9edbc6faf3f3ec
Fredy Aristizabal <freddyarh13@gmail.com>
2024-03-06 16:25:27 +1100

    Compile typescript

--- dist/database/config.js
+++ dist/database/config.js
@@ -18,7 +18,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const dbConnection = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        yield mongoose_1.default.connect('mongodb://localhost:27017/diary_app');
+        yield mongoose_1.default.connect('mongodb+srv://user_node_disability:YoxQ7h6k2ZswBs35@disabilityappcluster.vd7ecej.mongodb.net/journal_back');
         console.log('Database online');
     }
     catch (error) {
